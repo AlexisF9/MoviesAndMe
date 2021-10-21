6e153437808dfe85e72fb571113431b2857c836d
Alexis Flacher <aflacher@fr-b-tb04878.local>
2021-10-21 21:20:56 +0200

    Style home

--- components/home.js
+++ components/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 
 export default function Home({navigation}){
 
@@ -8,9 +8,21 @@ export default function Home({navigation}){
     }
 
     return (
-        <View>
-            <Text>Home page</Text>
+        <View style={styles.mainHome}>
+            <Text style={styles.title}>Movies And Me</Text>
             <Button title="Page recherche" onPress={goToSearch}/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    mainHome: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    title: {
+        fontSize: 30,
+        margin: 20
+    }
+})
